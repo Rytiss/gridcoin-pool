75145c32ff087e43ba68abf5d5a85d4bc692fe50
Rytis Slatkevičius <rytis.s@gmail.com>
2017-08-22 16:27:28 +0300

    Remove unused autogenerated code

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,7 +10,6 @@ use App\Controller\AppController;
  * @method \App\Model\Entity\Admin[] paginate($object = null, array $settings = [])
  */
 class AdminController extends AppController {
-
     /**
      * Index method
      *
@@ -39,88 +38,5 @@ class AdminController extends AppController {
             $this->set('daemonBalance', $info[0]);
         }
         $this->set('daemonRunning', $daemonRunning);
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id Admin id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $admin = $this->Admin->get($id, [
-            'contain' => []
-        ]);
-
-        $this->set('admin', $admin);
-        $this->set('_serialize', ['admin']);
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $admin = $this->Admin->newEntity();
-        if ($this->request->is('post')) {
-            $admin = $this->Admin->patchEntity($admin, $this->request->getData());
-            if ($this->Admin->save($admin)) {
-                $this->Flash->success(__('The admin has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The admin could not be saved. Please, try again.'));
-        }
-        $this->set(compact('admin'));
-        $this->set('_serialize', ['admin']);
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Admin id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $admin = $this->Admin->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $admin = $this->Admin->patchEntity($admin, $this->request->getData());
-            if ($this->Admin->save($admin)) {
-                $this->Flash->success(__('The admin has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The admin could not be saved. Please, try again.'));
-        }
-        $this->set(compact('admin'));
-        $this->set('_serialize', ['admin']);
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Admin id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $admin = $this->Admin->get($id);
-        if ($this->Admin->delete($admin)) {
-            $this->Flash->success(__('The admin has been deleted.'));
-        } else {
-            $this->Flash->error(__('The admin could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
     }
 }
